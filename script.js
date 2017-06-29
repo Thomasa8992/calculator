38e5a24c394dce9a9ba36093cd11ff6e3bdca1a0
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-06-29 16:53:03 -0500

    Added logic for num and spans along with clear

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+var clearBtn = $('#clear'),
+addBtn = $('#add')
+numArray = [];
 $(document).ready(function(){
-    alert();
+    handleSpan();
+    handleNum();
+    handleClear()
 });
+
+function handleNum(){
+    $('.num').on('mousedown', function() {
+        var numText = $(this).text();
+        $('#readout').append(numText);
+    });   
+}
+function handleSpan(){
+    $('span').on('mousedown', function() {
+        $(this).addClass("highlight");
+    });        
+    $('span').on('mouseup', function(){
+        $(this).removeClass("highlight");
+    });
+}
+function handleClear(){
+    $(clearBtn).on('click', function(){
+    $('#readout').empty();
+    })
+}
+
+function handlePlus(a , b){
+    $(addbtn).on('click', function(){
+        console.log(addBtn);
+    })
+}
+    
+
+        
